79eed134a3a7b657a6ef014d569c748e1f2ddabc
danknjoroge <dank8505@gmail.com>
2021-12-19 23:17:08 +0300

    added email and message validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,11 @@
 //form validation
 function validateForm() {
     if (document.myForm.name.value === "") {
-        alert("Please enter yourname")
-        
+        alert("Please enter your Name");
     } else if(document.myForm.email.value === "") {
-        
+        alert("Please enter your Email");
     }else if(document.myForm.message.value === "") {
-
+        alert("Please enter your Message");
     }else{
         return true;
     }
