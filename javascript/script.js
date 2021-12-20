ada455306c95b44efdd7c06ad7daf5a3dc1a1d99
danknjoroge <dank8505@gmail.com>
2021-12-20 13:28:22 +0300

    added take data function

--- javascript/script.js
+++ javascript/script.js
@@ -8,10 +8,10 @@ function validateForm() {
         alert("Please enter your Message")
     }else{
         return true;
-    }
-    
+    } 
 }
 
+//user interface logic
 $(document).ready(function() {
     $("#imgd").click(function() {
         $(".desp").toggle();
@@ -30,12 +30,31 @@ $(document).ready(function() {
         })
     })
     $("#prodimg").click(function() {
-        $(".prodp").toggle();
-        $("#prodimg").hide();
+        $(".prodp").toggle("slow");
+        $("#prodimg").hide("slow");
         $(".prodp").click(function() {
-            $("#prodimg").show();
-            $(".prodp").hide();
+            $("#prodimg").show("slow");
+            $(".prodp").hide("slow");
         })
     })
+    $(".work1").mouseover(function() {
+        $(".work1-overlay").show("slow");
+    }).mouseout(function() {
+        $(".work1-overlay").hide("slow");
+
+    })
 });
+
+//business logic
+var takeData = document.getElementById("myForm");
+takeData.addEventListener("submit", (e) => {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message= document.getElementById("message").value;
+    if (name != "" && message !="") {
+        alert( name + " " +"We have received your message. Thank you for reaching out to us");
+    };
+    document.getElementById("myForm").reset();
+});
+
 
